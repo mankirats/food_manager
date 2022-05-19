31eb48647c47c1f3b251708071e6cf921df2a04a
Mankirat <mankirat95@gmail.com>
2022-05-19 22:00:50 +0530

    Top Dishes component added

---
+++ src/components/TopDishes.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+    TopDishesBackground,
+    StyledTitle,
+    TopDishItemSpan,
+} from "./CustomStyle";
+function TopDishes(props) {
+    return (
+        <>
+            <StyledTitle>Top Food Recipes</StyledTitle>
+            <TopDishesBackground>
+                <TopDishItemSpan>mkmklm</TopDishItemSpan>
+                <TopDishItemSpan>mkmklm</TopDishItemSpan>
+                <TopDishItemSpan>mkmklm</TopDishItemSpan>
+                <TopDishItemSpan>mkmklm</TopDishItemSpan>
+                <TopDishItemSpan>mkmklm</TopDishItemSpan>
+                <TopDishItemSpan>mkmklm</TopDishItemSpan>
+                <TopDishItemSpan>mkmklm</TopDishItemSpan>
+            </TopDishesBackground>
+        </>
+    );
+}
+
+export default TopDishes;
