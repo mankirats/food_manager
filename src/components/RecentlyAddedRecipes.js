3260d605cfcedeca3e59bbbe1323aaf6a961f65d
Mankirat <mankirat95@gmail.com>
2022-05-19 22:00:06 +0530

    New component added

---
+++ src/components/RecentlyAddedRecipes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyledTitle } from "./CustomStyle";
+function RecentlyAddedRecipes(props) {
+    return (
+        <>
+            <StyledTitle>Recently Added Recipes</StyledTitle>
+        </>
+    );
+}
+
+export default RecentlyAddedRecipes;
