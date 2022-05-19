c625568c060db7b74124f802c7d518a040aef183
Mankirat <mankirat95@gmail.com>
2022-05-19 22:01:53 +0530

    App js new components merged

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
+import RecipeIndex from "./components/RecipeIndex";
+import TopDishes from "./components/TopDishes";
+import RecentlyAddedRecipes from "./components/RecentlyAddedRecipes";
 function App() {
     return (
         <div className="App">
             <Header></Header>
+            <RecipeIndex></RecipeIndex>
+            <TopDishes></TopDishes>
+            <RecentlyAddedRecipes></RecentlyAddedRecipes>
         </div>
     );
 }
