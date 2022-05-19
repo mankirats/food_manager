0830c89f6003320e77baf2c50c50f209a8f905d3
Mankirat <mankirat95@gmail.com>
2022-05-19 22:00:23 +0530

    src\components\RecipeIndex.js

---
+++ src/components/RecipeIndex.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { RecipeIndexBackground, RecipeCategory } from "./CustomStyle";
+function RecipeIndex(props) {
+    return (
+        <>
+            <RecipeIndexBackground>
+                <RecipeCategory>Indian Cuisine</RecipeCategory>
+                <RecipeCategory>RecipeIndex</RecipeCategory>
+                <RecipeCategory>RecipeIndex</RecipeCategory>
+                <RecipeCategory>RecipeIndex</RecipeCategory>
+                <RecipeCategory>RecipeIndex</RecipeCategory>
+                <RecipeCategory>RecipeIndex</RecipeCategory>
+                <RecipeCategory>RecipeIndex</RecipeCategory>
+                <RecipeCategory>RecipeIndex</RecipeCategory>
+            </RecipeIndexBackground>
+        </>
+    );
+}
+
+export default RecipeIndex;
