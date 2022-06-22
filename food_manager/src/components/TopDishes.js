f28ce9fde7d50ff8fc414efe6987109d81877398
Mankirat <mankirat95@gmail.com>
2022-06-22 18:56:23 +0530

    Added listing request

--- food_manager/src/components/TopDishes.js
+++ food_manager/src/components/TopDishes.js
@@ -1,22 +1,20 @@
 import React from "react";
+import axios from "axios";
 import {
     TopDishesBackground,
     StyledTitle,
     TopDishItemSpan,
 } from "./CustomStyle";
-function TopDishes(props) {
+async function TopDishes(props) {
+    const restaurantsData = await axios.get("localhost:5000/api/restaurant");
+
+    const topRestaurants = restaurantsData.map((op) => (
+        <TopDishItemSpan>{op}</TopDishItemSpan>
+    ));
     return (
         <>
             <StyledTitle>Top Rated Restaurants Near You</StyledTitle>
-            <TopDishesBackground>
-                <TopDishItemSpan>mkmklm</TopDishItemSpan>
-                <TopDishItemSpan>mkmklm</TopDishItemSpan>
-                <TopDishItemSpan>mkmklm</TopDishItemSpan>
-                <TopDishItemSpan>mkmklm</TopDishItemSpan>
-                <TopDishItemSpan>mkmklm</TopDishItemSpan>
-                <TopDishItemSpan>mkmklm</TopDishItemSpan>
-                <TopDishItemSpan>mkmklm</TopDishItemSpan>
-            </TopDishesBackground>
+            <TopDishesBackground>{restaurantsData}</TopDishesBackground>
         </>
     );
 }
