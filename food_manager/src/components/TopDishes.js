cb3b6d6800b1b23a6740fff11c3edcdc59e22633
Mankirat <mankirat95@gmail.com>
2022-06-23 22:48:30 +0530

    Backend request added

--- food_manager/src/components/TopDishes.js
+++ food_manager/src/components/TopDishes.js
@@ -1,20 +1,40 @@
-import React from "react";
-import axios from "axios";
 import {
     TopDishesBackground,
     StyledTitle,
     TopDishItemSpan,
 } from "./CustomStyle";
-async function TopDishes(props) {
-    const restaurantsData = await axios.get("localhost:5000/api/restaurant");
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-    const topRestaurants = restaurantsData.map((op) => (
-        <TopDishItemSpan>{op}</TopDishItemSpan>
+function TopDishes(props) {
+    const [restaurantData, setRestaurantData] = useState([
+        "Chipotle",
+        "Panera",
+        "Cookout",
+    ]);
+    useEffect(() => {
+        getRestaurantsData();
+    }, []);
+    const getRestaurantsData = async () => {
+        try {
+            const getRestaurantsData = await axios.get(
+                "http://localhost:5000/api/restaurant"
+            );
+            const loadedData = getRestaurantsData.data;
+            setRestaurantData(loadedData);
+            // return loadedData;
+        } catch (err) {
+            console.log(err.message);
+        }
+    };
+    // setRestaurantData(getRestaurants());
+    const data = restaurantData.map((op, index) => (
+        <TopDishItemSpan key={index}>{op}</TopDishItemSpan>
     ));
     return (
         <>
             <StyledTitle>Top Rated Restaurants Near You</StyledTitle>
-            <TopDishesBackground>{restaurantsData}</TopDishesBackground>
+            <TopDishesBackground>{data}</TopDishesBackground>
         </>
     );
 }
