a6610756749290691562febb41971fd6e8b2a0e3
Mankirat <mankirat95@gmail.com>
2022-06-26 00:06:12 +0530

    Route added for restaurant component

--- food_manager/src/App.js
+++ food_manager/src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import HomePage from "./components/HomePage";
+import Restaurant from "./components/Restaurant";
+import Header from "./components/Header";
 
 import { Routes, Route } from "react-router-dom";
+
 function App() {
     return (
         <div className="App">
+            <Header key="header"></Header>
             <Routes>
                 <Route path="/" element={<HomePage />}></Route>
+                <Route path="/Restaurant" element={<Restaurant />}></Route>
             </Routes>
         </div>
     );
